96b7c43e68696bd64871b930d36978b54cc647fc
Carlos Daniel <carlosporrasayala@gmail.com>
2024-07-31 06:49:49 -0500

    Display Page heading and breadcrumb

--- Expense Tracker/Controllers/CategoriesController.cs
+++ Expense Tracker/Controllers/CategoriesController.cs
@@ -19,24 +19,6 @@ namespace Expense_Tracker.Controllers
                 View(await _context.Categories.ToListAsync()) :
                 View(Enumerable.Empty<Category>());
 
-        // GET: Categories/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
         // GET: Categories/Create
         public IActionResult Create() => View(new Category());
         
@@ -104,24 +86,6 @@ namespace Expense_Tracker.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(category);
-        }
-
-        // GET: Categories/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
             return View(category);
         }
 
